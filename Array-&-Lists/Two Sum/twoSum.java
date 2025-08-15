5ebedf95b000fe3f4f7c92f2753df8009732442c
Tejas Jogdand <tejasjogdand37@gmail.com>
2025-08-15 14:52:08 +0530

    brutforce method

--- Array-&-Lists/Two Sum/twoSum.java
+++ Array-&-Lists/Two Sum/twoSum.java
@@ -1,5 +1,25 @@
 public class twoSum {
+
+	static void brutForce(int arr[], int targetSum){
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if(targetSum==arr[i]+arr[j]){
+					System.out.println("("+arr[i]+","+arr[j]+")");
+					break;
+				}
+			}
+		}
+	}
+
+	static void twoPointer(int arr[], int targetSum){
+		
+	}
+
 	public static void main(String[] args){
-		System.out.println("Here I'll code twoSum problem");
+		// int arr[] = {2,3,4,12,1};
+		int arr[] = {1,2,3,4,5};
+		int targetSum = 5;
+		brutForce(arr, targetSum);
+		
 	}
 }
