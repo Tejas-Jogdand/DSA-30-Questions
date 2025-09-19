8ef73536a9983027045ad52ca35b8647e2a75a26
Tejas Jogdand <tejasjogdand37@gmail.com>
2025-09-19 23:50:27 +0530

    First and last position of element in array

---
+++ Array-&-Lists/First & Last Pos/FLPos.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class FLPos {
+    public static int[] searchRange(int[] nums, int target) {
+        int first = findBound(nums, target, true);
+        int last = findBound(nums, target, false);
+        return new int[]{first, last};
+    }
+
+    private static int findBound(int[] nums, int target, boolean isFirst) {
+        int left = 0, right = nums.length - 1, result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                result = mid;
+                if (isFirst) {
+                    right = mid - 1; // keep searching left
+                } else {
+                    left = mid + 1; // keep searching right
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }    
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,5,6};
+        System.out.println(Arrays.toString(searchRange(arr,3)));
+    }
+}
